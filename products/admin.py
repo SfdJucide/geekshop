248735aec9e677c736efba1eaa3f57891cc4d4a5
sfd <kater167761@gmail.com>
2021-10-31 23:50:47 +0300

    continue hw7

--- products/admin.py
+++ products/admin.py
@@ -3,4 +3,8 @@ from products.models import ProductCategory, Product
 # Register your models here.
 
 admin.site.register(ProductCategory)
-admin.site.register(Product)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'quantity', 'category')
