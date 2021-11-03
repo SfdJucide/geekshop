4cf613c0e99a972a1caac55e1ee7f3a0b0552ff8
sfd <kater167761@gmail.com>
2021-11-03 17:51:05 +0300

    continue hw8

--- admins/views.py
+++ admins/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render, HttpResponseRedirect
+from django.utils.decorators import method_decorator
+
 from users.models import User
 from admins.forms import UserAdminRegistrationForm, UserAdminProfileForm
-from django.urls import reverse
+from django.urls import reverse_lazy
 from django.contrib.auth.decorators import user_passes_test
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 
 @user_passes_test(lambda user: user.is_staff)
@@ -11,48 +15,100 @@ def index(request):
     return render(request, 'admins/index.html', context)
 
 
-@user_passes_test(lambda user: user.is_staff)
-def admin_users(request):
-    context = {'title': 'Admin-Panel - Users',
-               'users': User.objects.all()
-               }
-    return render(request, 'admins/admin-users-read.html', context)
+class UserListView(ListView):
+    model = User
+    template_name = 'admins/admin-users-read.html'
+
+    @method_decorator(user_passes_test(lambda u: u.is_staff))
+    def dispatch(self, request, *args, **kwargs):
+        return super(UserListView, self).dispatch(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super(UserListView, self).get_context_data(**kwargs)
+        context['title'] = 'Admin-Panel - Users'
+        return context
 
 
-@user_passes_test(lambda user: user.is_staff)
-def admin_users_create(request):
-    if request.method == 'POST':
-        form = UserAdminRegistrationForm(data=request.POST, files=request.FILES)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect(reverse('admins:admin_users'))
-    else:
-        form = UserAdminRegistrationForm()
-    context = {'title': 'Admin-Panel - Creation user',
-               'form': form}
-    return render(request, 'admins/admin-users-create.html', context)
+class UserCreateView(CreateView):
+    model = User
+    form_class = UserAdminRegistrationForm
+    success_url = reverse_lazy('admins:admin_users')
+    template_name = 'admins/admin-users-create.html'
+
+    @method_decorator(user_passes_test(lambda u: u.is_staff))
+    def dispatch(self, request, *args, **kwargs):
+        return super(UserCreateView, self).dispatch(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super(UserCreateView, self).get_context_data(**kwargs)
+        context['title'] = 'Admin-Panel - Creation user'
+        return context
 
 
-@user_passes_test(lambda user: user.is_staff)
-def admin_users_update(request, id):
-    selected_user = User.objects.get(id=id)
-    if request.method == 'POST':
-        form = UserAdminProfileForm(instance=selected_user, files=request.FILES, data=request.POST)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect(reverse('admins:admin_users'))
-    else:
-        form = UserAdminProfileForm(instance=selected_user)
-    context = {'title': 'Admin-Panel - Edit user',
-               'form': form,
-               'selected_user': selected_user,
-               }
-    return render(request, 'admins/admin-users-update-delete.html', context)
+class UserUpdateView(UpdateView):
+    model = User
+    form_class = UserAdminProfileForm
+    success_url = reverse_lazy('admins:admin_users')
+    template_name = 'admins/admin-users-update-delete.html'
+
+    @method_decorator(user_passes_test(lambda u: u.is_staff))
+    def dispatch(self, request, *args, **kwargs):
+        return super(UserUpdateView, self).dispatch(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super(UserUpdateView, self).get_context_data(**kwargs)
+        context['title'] = 'Admin-Panel - Edit user'
+        return context
 
 
-@user_passes_test(lambda user: user.is_staff)
-def admin_users_delete(request, id):
-    user = User.objects.get(id=id)
-    user.is_active = False
-    user.save()
-    return HttpResponseRedirect(reverse('admins:admin_users'))
+class UserDeleteView(DeleteView):
+    model = User
+    template_name = 'admins/admin-users-update-delete.html'
+    success_url = reverse_lazy('admins:admin_users')
+
+
+# @user_passes_test(lambda user: user.is_staff)
+# def admin_users(request):
+#     context = {'title': 'Admin-Panel - Users',
+#                'users': User.objects.all()
+#                }
+#     return render(request, 'admins/admin-users-read.html', context)
+#
+#
+# @user_passes_test(lambda user: user.is_staff)
+# def admin_users_create(request):
+#     if request.method == 'POST':
+#         form = UserAdminRegistrationForm(data=request.POST, files=request.FILES)
+#         if form.is_valid():
+#             form.save()
+#             return HttpResponseRedirect(reverse('admins:admin_users'))
+#     else:
+#         form = UserAdminRegistrationForm()
+#     context = {'title': 'Admin-Panel - Creation user',
+#                'form': form}
+#     return render(request, 'admins/admin-users-create.html', context)
+#
+#
+# @user_passes_test(lambda user: user.is_staff)
+# def admin_users_update(request, id):
+#     selected_user = User.objects.get(id=id)
+#     if request.method == 'POST':
+#         form = UserAdminProfileForm(instance=selected_user, files=request.FILES, data=request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return HttpResponseRedirect(reverse('admins:admin_users'))
+#     else:
+#         form = UserAdminProfileForm(instance=selected_user)
+#     context = {'title': 'Admin-Panel - Edit user',
+#                'form': form,
+#                'selected_user': selected_user,
+#                }
+#     return render(request, 'admins/admin-users-update-delete.html', context)
+#
+#
+# @user_passes_test(lambda user: user.is_staff)
+# def admin_users_delete(request, id):
+#     user = User.objects.get(id=id)
+#     user.is_active = False
+#     user.save()
+#     return HttpResponseRedirect(reverse('admins:admin_users'))
--- products/views.py
+++ products/views.py
@@ -3,7 +3,7 @@ import os
 
 from django.shortcuts import render
 from products.models import Product, ProductCategory
-
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 # Create your views here.
 
 
@@ -14,10 +14,21 @@ def index(request):
     return render(request, 'products/index.html', context)
 
 
-def products(request):
+def products(request, category_id=None, page=1):
     context = {
         "title": 'GeekShop - Каталог',
-        "product_list": Product.objects.all(),
         "categories_list": ProductCategory.objects.all(),
     }
+    if category_id:
+        product = Product.objects.filter(category_id=category_id)
+    else:
+        product = Product.objects.all()
+    paginator = Paginator(product, 3)
+    try:
+        products_paginator = paginator.page(page)
+    except PageNotAnInteger:
+        products_paginator = paginator.page(1)
+    except EmptyPage:
+        products_paginator = paginator.page(paginator.num_pages)
+    context['product_list'] = products_paginator
     return render(request, 'products/products.html', context)
